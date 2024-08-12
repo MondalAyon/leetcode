725d97817e4b391fa141cf32109fc9a6be18cfde
MondalAyon <ayonmondal@yahoo.co.in>
2024-08-12 15:24:10 -0700

    Create convertToTitle.java

---
+++ convertToTitle.java
@@ -0,0 +1,50 @@
+public class convertToTitle {
+
+    public static void main (String[] args) {
+        int inputcolumnNumber = 1;
+        //int inputcolumnNumber = 77;
+        String columnTitle = "";
+
+        int columnNumber = inputcolumnNumber;
+        int offset = 64;
+        int q = -1;
+        int r = -1;
+
+        //System.out.println((int)c);
+        //System.out.println((char)65);
+
+        //System.out.println(52/26);
+
+        if (columnNumber >= 1 && columnNumber <= 26) {
+            columnTitle = String.valueOf((char)(columnNumber + offset));
+        }
+        else if (columnNumber > 26) {
+
+            while (true) {
+
+                r = columnNumber % 26;
+                q = columnNumber / 26;
+                System.out.println("q=" + q + " r=" + r);
+
+                if (r == 0) {
+                    q--;
+                    r=26;
+                    System.out.println("Changed q=" + q + " r=" + r);
+                }
+
+                if (q <= 26) {
+                    columnTitle =  String.valueOf((char)(r + offset)) + columnTitle;
+                    columnTitle =  String.valueOf((char)(q + offset)) + columnTitle;
+                    break;
+                }
+                else {
+                    columnTitle = String.valueOf((char)(r + offset)) + columnTitle;
+                    columnNumber = q;
+                }
+            } // eod of while
+        }
+
+        System.out.println("inputcolumnNumber = " + inputcolumnNumber + " columnTitle = " + columnTitle);
+
+    }
+}
